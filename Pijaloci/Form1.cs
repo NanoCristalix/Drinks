13cef3fb81b0eb4d5a7c708d779dd6889eb871c0
Nenad Hristov <hristov_nenad@hotmail.com>
2021-04-28 23:42:11 +0200

    First commit

---
+++ Pijaloci/Form1.cs
@@ -0,0 +1,169 @@
+﻿using Pijaloci.PijalociClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pijaloci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PijalociClass c = new PijalociClass();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //GET the value from input fields
+            c.Price = textBox2.Text;
+            c.DrinkName = textBox3.Text;
+            c.TableNo = textBox4.Text;
+
+            //inserting data into Database
+            bool success = c.Insert(c);
+            if(success==true)
+            {
+                //successfully inserted
+                MessageBox.Show("Noviot pijalok uspesno vnesen");
+                //Povikaj Clear Methoda
+                Clear();
+
+            }
+            else
+            {
+                //Failed to add Pijalok
+                MessageBox.Show("Neuspesno vnesuvanje na pijalok");
+            }
+
+            //Load Data on Data GridView
+            DataTable dt = c.Select();
+            dataGridView1.DataSource = dt;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load Data on Data GridView
+            DataTable dt = c.Select();
+            dataGridView1.DataSource = dt;
+        }
+
+        //methoda za briseknje na podatocite posle vnesuvanjeto od textbox
+        public void Clear()
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //get the data from textboxes
+            c.DrinkID = int.Parse(textBox1.Text);
+            c.Price = textBox2.Text;
+            c.DrinkName = textBox3.Text;
+            c.TableNo = textBox4.Text;
+
+            //Update data in database
+            bool success = c.Update(c);
+            if(success==true)
+            {
+                //Update uspesno
+                MessageBox.Show("Na Pijalokot e upesno napraveno update");
+                //Load Data on Data GridView
+                DataTable dt = c.Select();
+                dataGridView1.DataSource = dt;
+
+                //Call Clear Method
+                Clear();
+
+
+            }
+            else
+            {
+                //Update neuspeno
+                MessageBox.Show("Update na pijalokot e neuspesno");
+         
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //get the data from data grid view and load it to the textboxes respectively
+            //identify the row on whitch mouse is clicked
+            int rowIndex = e.RowIndex;
+            textBox1.Text = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[rowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[rowIndex].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[rowIndex].Cells[3].Value.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Call clear method here
+            Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Get the DrinkID from the application
+            c.DrinkID = Convert.ToInt32(textBox1.Text);
+            bool success = c.Delete(c);
+            if(success==true)
+            {
+                //Uspeno izbrisano
+                MessageBox.Show("Pijalokot e uspesno izbrisan");
+                //Refresh Data GridWiew
+                //Load Data on Data GridView
+                DataTable dt = c.Select();
+                dataGridView1.DataSource = dt;
+
+                //Call the Clear Method
+                Clear();
+
+
+            }
+            else
+            {
+                //Neuspeno izbrisano
+                MessageBox.Show("Pijalokot ne e izbrisan");
+            }
+
+        }
+        static string mykonekcija = ConfigurationManager.ConnectionStrings["Konekcija"].ConnectionString;
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            //get the value from textbox
+            string keyword = textBox5.Text;
+
+            SqlConnection conn = new SqlConnection(mykonekcija);
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT *FROM tbl_pijaloci WHERE Price LIKE '%"+keyword+ "%' OR DrinkName LIKE '%" + keyword + "%' OR TableNo LIKE '%" + keyword + "%' ", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
---
+++ Pijaloci/PijalociClasses/PijalociClass.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pijaloci.PijalociClasses
+{
+    class PijalociClass
+    {
+        //get i set properties
+        public int DrinkID { get; set; }
+        public string Price { get; set; }
+        public string DrinkName { get; set; }
+        public string TableNo { get; set; }
+
+        static string mykonekcija = ConfigurationManager.ConnectionStrings["Konekcija"].ConnectionString; 
+
+       
+        //Selektiranje na datata od Database   
+        public DataTable Select()
+        {
+            //Cekor1: Database Connection
+            SqlConnection conn = new SqlConnection(mykonekcija);
+            DataTable dt = new DataTable();
+
+            try
+            {
+
+                //Cekor2: Writing SQL query
+                string sql = "SELECT *FROM tbl_pijaloci";
+                //Creating cmd using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creating DataAdapter using cmd
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+
+
+        }
+
+        //Inserting Data into Database
+        public bool Insert (PijalociClass c)
+        {
+            //Create a default return type and settings its value to false
+            bool isSuccess = false;
+
+            //Cekor1: Connect DataBase
+            SqlConnection conn = new SqlConnection(mykonekcija);
+            try
+            {
+                //Cekor2:Create Sql Query to insert data
+
+                string sql = " INSERT INTO tbl_pijaloci (Price, DrinkName, TableNo) VALUES (@Price, @DrinkName, @TableNo)";
+
+                //creating sql command  using sql and conn
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create Parameters to add Data
+
+                cmd.Parameters.AddWithValue("@Price", c.Price);
+                cmd.Parameters.AddWithValue("@DrinkName", c.DrinkName);
+                cmd.Parameters.AddWithValue("TableNo", c.TableNo);
+
+                //Connection Opeh Here
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                
+                //If the query runs succesfully then the value of rows will be greater then zero  else its value will be 0
+                if(rows>0)
+                {
+                    isSuccess = true;
+
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+
+        }
+
+        //Method to update data in database from our application
+
+        public bool Update(PijalociClass c)
+        {
+            //Create a default return type and set its default value to false
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(mykonekcija);
+            try
+            {
+                //SQL to update data in our database 
+                string sql = "UPDATE tbl_pijaloci SET Price=@Price, DrinkName=@DrinkName, TableNo=@TableNo WHERE DrinkID=@DrinkID";
+
+                //Creating SQL command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create Parameters to add value
+                cmd.Parameters.AddWithValue("@Price", c.Price);
+                cmd.Parameters.AddWithValue("@DrinkName", c.DrinkName);
+                cmd.Parameters.AddWithValue("@TableNo", c.TableNo);
+                cmd.Parameters.AddWithValue("DrinkID", c.DrinkID);
+
+                //Open Database Connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                //if the query runs succesfully then the value of  rows will be greater then null else its value will be zero
+                if(rows>0)
+                {
+                    isSuccess = true;
+
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+
+            catch(Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+
+        }
+
+        //Method to delete data from database
+        public bool Delete(PijalociClass c)
+        {
+            //create default return value and set its value to false
+            bool isSuccess = false;
+
+            //Create sql connection
+            SqlConnection conn = new SqlConnection(mykonekcija);
+            try
+            {
+                //SQl to detele data
+                string sql = "DELETE FROM tbl_pijaloci WHERE DrinkID=@DrinkID";
+
+                //Creating sql command 
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@DrinkID", c.DrinkID);
+
+                //Open connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                //if the query runs succesfully then the value of  rows will be greater then null else its value will be zero
+                if(rows>0)
+                {
+                    isSuccess = true;
+
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                //close connection
+                conn.Close();
+            }
+            return isSuccess;
+
+        }
+
+
+
+
+
+    }
+}
